6ab6c9605bc28d10d7419d4cfa2478e8a6843919
Edgar Hernandez <edgarchortiz@gmail.com>
2018-02-20 13:27:59 -0800

    fix html and readme.

--- js/spaceAge-interface.js
+++ js/spaceAge-interface.js
@@ -13,14 +13,31 @@ $(document).ready(function() {
     let jupiter = age.ageJupiter();
     let jupiterExpectancy = age.lifeExpectancyJupiter();
     let earth = age.ageInSeconds();
+    $('#mercuryAge').empty(mercury);
     $('#mercuryAge').append(mercury);
+
+    $('#mercuryExpectancy') .empty(mercuryExpectancy);
     $('#mercuryExpectancy').append(mercuryExpectancy);
+
+    $('#venusAge').empty(venus);
     $('#venusAge').append(venus);
+
+    $('#venusExpectancy').empty(venusExpectancy);
     $('#venusExpectancy').append(venusExpectancy);
+
+    $('#marsAge').empty(mars);
     $('#marsAge').append(mars);
+
+    $('#marsExpectancy').empty(marsExpectancy);
     $('#marsExpectancy').append(marsExpectancy);
+
+    $('#jupiterAge' + "years").empty(jupiter);
     $('#jupiterAge').append(jupiter);
+
+    $('#jupiterExpectancy').empty(jupiterExpectancy);
     $('#jupiterExpectancy').append(jupiterExpectancy);
+
+    $('#ageSeconds').empty(earth);
     $('#ageSeconds').append(earth);//ageInSeconds
   });
 });
